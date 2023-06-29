ae929cd496dc588ef24dfcb7b88630f607f93d2e
Aviral Petwal <avpe@pop-os.localdomain>
2023-06-29 11:06:37 -0400

    adding support for multiple products in purchase order

--- materials_castelar/models/stock_picking_inherit.py
+++ materials_castelar/models/stock_picking_inherit.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models, Command
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
 
 class StockPickingInherit(models.Model):
     _inherit = "stock.picking"
@@ -10,7 +10,8 @@ class StockPickingInherit(models.Model):
             state = self.env["purchase.order"].search([("name", "=", picking.origin)]).state
             if state== "purchase":
                 stock_move_record=self.env["stock.move"].search([("picking_id", "=", picking.id)])
-                if stock_move_record.product_uom_qty < stock_move_record.quantity_done:
-                    raise ValidationError("You can't receive more than the ordered quantity. Please, enter another quantity")
+                for stock_move in stock_move_record:
+                    if stock_move.product_uom_qty < stock_move.quantity_done:
+                        raise UserError("You can't receive more than the ordered quantity. Please, enter another quantity")
         res = super(StockPickingInherit, self).button_validate()
         return res
